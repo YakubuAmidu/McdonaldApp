976896e181662b94ed5bb60afebc54ff05c4d020
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-05 00:40:18 -0700

    Commiting changes

---
+++ components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Logo from './Logo';
+import { View, Text, StyleSheet } from 'react-native';
+
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+      <Logo />
+      </View>
+    )
+  }
+}
+
+
+export default App;
---
+++ components/Logo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+class Logo extends Component {
+  render() {
+    return (
+      <View style={{ alignItems: 'center', top: 40, paddingBottom: 5, borderBottomWidth: 1, borderColor: '#a0a0a0' }}>
+      <Text>M</Text>
+      </View>
+    )
+  }
+}
+
+export default Logo;
