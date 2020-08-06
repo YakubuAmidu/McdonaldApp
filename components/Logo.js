c40b09793c8909c2fc931befa327daadebcea428
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-06 07:46:59 -0700

    Commiting changes

--- components/Logo.js
+++ components/Logo.js
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet, Image } from 'react-native';
 class Logo extends Component {
   render() {
     return (
-      <View style={{ alignItems: 'center', top: 50, paddingBottom: 5, borderBottomWidth: 1, borderColor: '#a0a0a0' }}>
+      <View style={{ alignItems: 'center', top: 50, paddingBottom: 5}}>
       <Image style={{ width: 35, height: 30}} source={require('../assets/logo.jpg')} />
       </View>
     )
--- components/Scroll.js
+++ components/Scroll.js
@@ -10,9 +10,11 @@ class Scroll extends Component {
       <Image style={{ width: '100%', height: 190, resizeMode: 'stretch', position: 'relative'}} source={require('../assets/burger2.jpg')} />
       <Text style={{ left: 15, position: 'absolute', fontSize: 30, top: 20, fontWeight: '700' }}>Cheese bits on</Text>
       <Text style={{ left: 15, position: 'absolute', fontSize: 30, top: 50, fontWeight: '700' }}>the wrapper</Text>
-      
+      <Text style={{ position: 'absolute', top: 85, left: 15 }}>Of the Sausage McMuffin with Egg</Text>
       <Text style={{ left: 15, textAlign: 'center', padding: 10, position: 'absolute', top: 120, backgroundColor: '#FAA41A'}}>Get Morning Started</Text>
       </View>
+
+      <Text style={{ fontSize: 10, fontWeight: 'normal', color: '#686868', marginTop: 10, marginBottom:  35, marginLeft: 15, marginRight: 15 }}> Mobile order & pay at participation Mcdonald. McD App registration required.{'\u00A9'}2020 Mcdonald's.</Text>
 
       <View style={{ marginLeft: 15, marginRight: 15, position: 'relative' }}>
       <Image style={{ width: '100%' }} source={require('../assets/burger1.jpg')} />
@@ -21,8 +23,6 @@ class Scroll extends Component {
       <Text style={{ backgroundColor: '#FAA41A', width: 150, position: 'absolute',
        top: 110, color: '#000',  textAlign: 'center', paddingTop: 12, paddingBottom: 12 }}>Start Order</Text>
       </View>
-
-      <Text style={{ fontSize: 10, fontWeight: 'normal', color: '#686868', marginTop: 10, marginBottom:  35, marginLeft: 15, marginRight: 15 }}> Mobile order & pay at participation Mcdonald. McD App registration required.{'\u00A9'}2020 Mcdonald's.</Text>
 
       <View style={{ marginLeft: 15, marginRight: 15, position: 'relative'  }}>
       <Text style={{ marginBottom: 20, fontWeight: '900', fontSize: 20 }}>Menu</Text>
