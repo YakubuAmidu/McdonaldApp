ac8b9f74225014aff9a8b7b1e6e985a4b82b5e6d
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-05 23:05:14 -0700

    Commiting changes

--- components/Logo.js
+++ components/Logo.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 class Logo extends Component {
   render() {
     return (
-      <View style={{ alignItems: 'center', top: 40, paddingBottom: 5, borderBottomWidth: 1, borderColor: '#a0a0a0' }}>
-      <Text style={{ fontWeight: 'bold', fontSize: 30, color: '#FFCC00' }}>M</Text>
+      <View style={{ alignItems: 'center', top: 50, paddingBottom: 5, borderBottomWidth: 1, borderColor: '#a0a0a0' }}>
+      <Image style={{ width: 35, height: 30}} source={require('../assets/logo.jpg')} />
       </View>
     )
   }
--- components/Scroll.js
+++ components/Scroll.js
@@ -4,8 +4,8 @@ import { View, StyleSheet, Text, ScrollView } from 'react-native';
 class Scroll extends Component {
   render() {
     return (
-      <ScrollView style={{ top: 40, height: '100%' }}>
-      <View>
+      <ScrollView style={{ top: 50, height: '100%' }}>
+      <View style={{ marginLeft: 15 }}>
        <Text>ScrollView</Text>
       </View>
       </ScrollView>
