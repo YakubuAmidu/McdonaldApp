3abdc4f2e0a17fcfc29ee8d50926476a718474c1
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-06 08:34:31 -0700

    Commiting changes

--- components/Scroll.js
+++ components/Scroll.js
@@ -11,7 +11,7 @@ class Scroll extends Component {
       <Text style={{ left: 15, position: 'absolute', fontSize: 30, top: 20, fontWeight: '700' }}>Cheese bits on</Text>
       <Text style={{ left: 15, position: 'absolute', fontSize: 30, top: 50, fontWeight: '700' }}>the wrapper</Text>
       <Text style={{ position: 'absolute', top: 85, left: 15 }}>Of the Sausage McMuffin with Egg</Text>
-      <Text style={{ left: 15, textAlign: 'center', padding: 10, position: 'absolute', top: 120, backgroundColor: '#FAA41A'}}>Get Morning Started</Text>
+      <Text style={{ left: 15, textAlign: 'center', padding: 10, position: 'absolute', top: 130, backgroundColor: '#FAA41A'}}>Get Morning Started</Text>
       </View>
 
       <Text style={{ fontSize: 10, fontWeight: 'normal', color: '#686868', marginTop: 10, marginBottom:  35, marginLeft: 15, marginRight: 15 }}> Mobile order & pay at participation Mcdonald. McD App registration required.{'\u00A9'}2020 Mcdonald's.</Text>
@@ -25,11 +25,13 @@ class Scroll extends Component {
        top: 100, color: '#000',  textAlign: 'center', paddingTop: 12, paddingBottom: 12 }}>Start Order</Text>
       </View>
 
+      <Text style={{ fontSize: 20, fontWeight: '900', marginTop: 30, marginBottom: 10, left: 15  }}>Deals</Text>
+
       <View style={{ marginLeft: 15, marginRight: 15, position: 'relative'  }}>
        <Image style={{ width: '100%' }} source={require('../assets/burger.jpg')} />
-       <Text style={{ fontSize: 30, fontWeight: '900', position: 'absolute', top: 70, left: 15 }}>Daily deals</Text>
-       <Text style={{ fontSize: 20, fontWeight: 'bold', position: 'absolute', top: 100, left: 15 }}>On everything you love</Text>
-       <Text style={{ position: 'absolute', paddingTop: 12, paddingBottom: 12, left: 15, top: 150, backgroundColor: '#fff',  width: 150, textAlign: 'center'}}>View Deals</Text>
+       <Text style={{ fontSize: 30, fontWeight: '900', position: 'absolute', top: 30, left: 15 }}>Daily deals</Text>
+       <Text style={{ fontSize: 20, fontWeight: 'bold', position: 'absolute', top: 60, left: 15 }}>On everything you love</Text>
+       <Text style={{ position: 'absolute', paddingTop: 12, paddingBottom: 12, left: 15, top: 100, backgroundColor: '#fff',  width: 150, textAlign: 'center'}}>View Deals</Text>
       </View>
 
       </ScrollView>
