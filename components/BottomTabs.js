9017b862a8c46ed5559bd32bbd2a4cbd18352b0e
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-09 02:16:20 -0700

    Commiting changes

--- components/BottomTabs.js
+++ components/BottomTabs.js
@@ -6,17 +6,17 @@ class BottomTabs extends Component {
     return (
       <View style={{ paddingLeft: 15, flexDirection: 'row', top: 53, alignItems: 'center', borderTopWidth: .5, borderColor: 'grey'}}>
 
-      <View style={{ top: 30, paddingRight: 20 }}>
+      <View style={{ top: 30, paddingRight: 30 }}>
       <Image style={{ width: 30, height: 30 }} source={require('../assets/logo2.png')} />
         <Text>Home</Text>
       </View>
 
-      <View style={{ top: 30 }}>
+      <View style={{ top: 30, paddingRight: 30 }}>
         <Image style={{ width: 30, height: 30 }} source={require('../assets/fries2.jpg')} />
         <Text>Order</Text>
       </View>
 
-      <View style={{ top: 30 }}>
+      <View style={{ top: 30, paddingRight: 30 }}>
         <Image style={{ width: 30, height: 30 }} source={require('../assets/deals.png')} />
         <Text>Deals</Text>
       </View>
