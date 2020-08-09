e507560fb373052e45665e76cd799b1e634ee9c8
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-09 02:01:50 -0700

    Commiting changes

--- components/BottomTabs.js
+++ components/BottomTabs.js
@@ -7,7 +7,7 @@ class BottomTabs extends Component {
       <View style={{ top: 53, alignItems: 'center', borderTopWidth: .5, borderColor: 'grey'}}>
 
       <View>
-      <Image source={require('../assets/logo2.png')} />
+      <Image style={{ width: 30, height: 30 }} source={require('../assets/logo2.png')} />
         <Text>Home</Text>
       </View>
 
