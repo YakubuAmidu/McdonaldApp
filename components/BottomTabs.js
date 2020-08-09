8573d334ca3eb13ccdbfafb2751321638b7e81f2
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-09 02:04:56 -0700

    Commiting changes

--- components/BottomTabs.js
+++ components/BottomTabs.js
@@ -4,15 +4,15 @@ import { View, Text, StyleSheet, Image } from 'react-native';
 class BottomTabs extends Component {
   render() {
     return (
-      <View style={{ top: 53, alignItems: 'center', borderTopWidth: .5, borderColor: 'grey'}}>
+      <View style={{ paddingLeft: 15, flexDirection: 'row', top: 53, alignItems: 'center', borderTopWidth: .5, borderColor: 'grey'}}>
 
-      <View>
+      <View style={{ paddingRight: 20 }}>
       <Image style={{ width: 30, height: 30 }} source={require('../assets/logo2.png')} />
         <Text>Home</Text>
       </View>
 
       <View>
-        <Image source={require('../assets/fries2.jpg')} />
+        <Image style={{ width: 30, height: 30 }} source={require('../assets/fries2.jpg')} />
         <Text>Order</Text>
       </View>
 
