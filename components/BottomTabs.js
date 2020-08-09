e6b858336670400954a8c99c29b58af518775288
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-09 02:43:01 -0700

    Commiting changes

--- components/BottomTabs.js
+++ components/BottomTabs.js
@@ -7,29 +7,29 @@ class BottomTabs extends Component {
     return (
       <View style={{ paddingLeft: 15, flexDirection: 'row', top: 53, alignItems: 'center', borderTopWidth: .5, borderColor: 'grey'}}>
 
-      <View style={{ top: 30, paddingRight: 35 }}>
+      <View style={{ top: 30, paddingRight: 40 }}>
       <Image style={{ width: 30, height: 30 }} source={require('../assets/logo2.png')} />
         <Text>Home</Text>
       </View>
 
-      <View style={{ top: 30, paddingRight: 35 }}>
+      <View style={{ top: 30, paddingRight: 40 }}>
         <Image style={{ width: 30, height: 30 }} source={require('../assets/fries2.jpg')} />
         <Text>Order</Text>
       </View>
 
-      <View style={{ top: 30, paddingRight: 35 }}>
+      <View style={{ top: 30, paddingRight: 40 }}>
         <Image style={{ width: 30, height: 30 }} source={require('../assets/deals.png')} />
         <Text>Deals</Text>
       </View>
 
-      <View style={{ top: 30, paddingRight: 35 }}>
+      <View style={{ top: 30, paddingRight: 40 }}>
         <Image style={{ width: 30, height: 30 }} source={require('../assets/recent.png')} />
         <Text>Recents</Text>
       </View>
 
-      <View>
-        <Text>000</Text>
-        <Text>More</Text>
+      <View style={{ top: 30 }}>
+        <Text style={{ fontSize: 10, fontWeight: 'bold'}}>0  0  0</Text>
+        <Text style={{ top: 10}}>More</Text>
       </View>
 
       </View>
