52ea0a9887f4e021f0bf374dcdad2d07693b46e3
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-08-09 02:37:50 -0700

    Commiting changes

--- components/BottomTabs.js
+++ components/BottomTabs.js
@@ -1,29 +1,35 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
+import Unorderedlist from 'react-native-unordered-list';
 
 class BottomTabs extends Component {
   render() {
     return (
       <View style={{ paddingLeft: 15, flexDirection: 'row', top: 53, alignItems: 'center', borderTopWidth: .5, borderColor: 'grey'}}>
 
-      <View style={{ top: 30, paddingRight: 30 }}>
+      <View style={{ top: 30, paddingRight: 35 }}>
       <Image style={{ width: 30, height: 30 }} source={require('../assets/logo2.png')} />
         <Text>Home</Text>
       </View>
 
-      <View style={{ top: 30, paddingRight: 30 }}>
+      <View style={{ top: 30, paddingRight: 35 }}>
         <Image style={{ width: 30, height: 30 }} source={require('../assets/fries2.jpg')} />
         <Text>Order</Text>
       </View>
 
-      <View style={{ top: 30, paddingRight: 30 }}>
+      <View style={{ top: 30, paddingRight: 35 }}>
         <Image style={{ width: 30, height: 30 }} source={require('../assets/deals.png')} />
         <Text>Deals</Text>
       </View>
 
-      <View>
+      <View style={{ top: 30, paddingRight: 35 }}>
         <Image style={{ width: 30, height: 30 }} source={require('../assets/recent.png')} />
         <Text>Recents</Text>
+      </View>
+
+      <View>
+        <Text>000</Text>
+        <Text>More</Text>
       </View>
 
       </View>
